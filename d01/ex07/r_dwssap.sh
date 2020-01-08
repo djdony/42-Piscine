04b08483ff7f079bd9a1daf6d7a77dae2df75e90
Daniyar Yusuf <dyusuf@e1z1r8p6.42.us.org>
2020-01-08 10:54:57 -0800

    ex07

--- d01/ex07/r_dwssap.sh
+++ d01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd
+cat /etc/passwd | sed -n 'n;p' | sed 's/:\(.\)//g' | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' |tr '\n' ',' | sed 's/,/, /g' | sed 's/\(.*\), /\1./' 
