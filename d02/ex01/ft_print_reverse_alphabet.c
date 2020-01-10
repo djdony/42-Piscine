b47280d7ad940e45613a8ecbce9ef566d86e11d0
Daniyar Yusuf <dyusuf@e1z1r2p9.42.us.org>
2020-01-09 20:47:15 -0800

    test

---
+++ d02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyusuf <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/09 17:00:35 by dyusuf            #+#    #+#             */
+/*   Updated: 2020/01/09 17:10:16 by dyusuf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter--;
+	}
+}
---
+++ d02/main.c
@@ -0,0 +1,14 @@
+//void ft_print_alphabet(void);
+//void ft_print_reverse_alphabet(void);
+//void ft_print_numbers(void);
+//void ft_is_negative(int);
+void ft_print_comb(int);
+
+int             main(void)
+{
+  //    ft_print_alphabet();
+//	ft_print_reverse_alphabet();
+//	ft_print_numbers();
+//	ft_is_negative(-3);
+	ft_print_comb();
+}
