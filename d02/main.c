8c57cf6898fd64aa84285d305bf92e05efec1a32
Daniyar Yusuf <dyusuf@e1z1r2p9.42.us.org>
2020-01-10 16:42:45 -0800

    ok

--- d02/main.c
+++ d02/main.c
@@ -2,13 +2,14 @@
 //void ft_print_reverse_alphabet(void);
 //void ft_print_numbers(void);
 //void ft_is_negative(int);
-void ft_print_comb(void);
-
+//void ft_print_comb(void);
+void ft_print_comb2(void);
 int             main(void)
 {
   //    ft_print_alphabet();
 //	ft_print_reverse_alphabet();
 //	ft_print_numbers();
 //	ft_is_negative(-3);
-	ft_print_comb();
+//	ft_print_comb();
+ft_print_comb2();
 }
