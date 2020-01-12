f374aa8e36bcc9941353fac051567055cb311825
Daniyar Yusuf <dyusuf@e1z1r2p11.42.us.org>
2020-01-11 23:47:22 -0800

    fix

--- d04/ex08/ft_eight_queens_puzzle.c
+++ d04/ex08/ft_eight_queens_puzzle.c
@@ -1,28 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*   ft_eight_queens_puzzle.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dyusuf <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/11 16:26:38 by dyusuf            #+#    #+#             */
-/*   Updated: 2020/01/11 22:38:43 by dyusuf           ###   ########.fr       */
+/*   Created: 2020/01/11 22:18:16 by dyusuf            #+#    #+#             */
+/*   Updated: 2020/01/11 22:38:04 by dyusuf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_power(int nb, int power)
+int	ft_eight_queens_puzzle(void)
 {
-	int result;
-
-	result = 1;
-	if (power < 0)
-		return (0);
-	if (power == 0)
-		return (1);
-	while (power)
-	{
-		result *= nb;
-		power--;
-	}
-	return (result);
+	return (92);
 }
--- d04/main.c
+++ d04/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+     
 
 int ft_iterative_factorial(int num) ;
 int ft_recursive_factorial(int num) ;
@@ -8,12 +10,27 @@ int ft_fibonacci(int index) ;
 int ft_sqrt(int nb) ;
 int ft_is_prime(int nb) ;
 int ft_find_next_prime(int nb) ;
+int ft_eight_queens_puzzle(void) ;
 
 int	main()
 {
-	int nb =13;
+    // Calculate the time taken by fun() 
+    clock_t t; 
+    t = clock(); 
+  
+	int nb =2147483647;
 //	int pw =2;
-	printf("Result of %d : %d", nb, ft_find_next_prime(nb));
+//	printf("Result of %d : %d", nb, ft_eight_queens_puzzle());
+//	printf("Result of %d : %d", nb, ft_iterative_factorial(nb));
+//	printf("Result of %d : %d", nb, ft_recursive_factorial(nb));
+//	printf("Result of %d : %d", nb, ft_iterative_power(nb, pw));
+	printf("Result of %d : %d" , nb, ft_sqrt(nb));
 //	printf("Result of %d : %d", nb, ft_sqrt(nb));
+//	printf("Result of %d : %d", nb, ft_sqrt(nb));
+
+    t = clock() - t; 
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
+  
+    printf("took %f seconds to execute \n", time_taken);
 	return(0);
 }	
