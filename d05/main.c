d4e7b2af645b14cb6846cea7bfa699113075a7a1
Daniyar Yusuf <dyusuf@e1z1r2p7.42.us.org>
2020-01-13 18:24:15 -0800

    atoi

--- d05/main.c
+++ d05/main.c
@@ -1,11 +1,16 @@
+#include <stdio.h>
+
 void	ft_putstr(char *str);
 void	ft_putchar(char c);
 void	ft_putnbr(int nb);
+int		ft_atoi(char *str);
 
 int		main(void)
 {
-//	char str1[]= "Hello";
+	char str1[] = "   -+42He55llo";
 
 //	ft_putstr(str1);
-    ft_putnbr(-2147483648);
+//    ft_putnbr(-2147483648);
+    int a = ft_atoi(str1);
+	printf ("%d", a);
 }
