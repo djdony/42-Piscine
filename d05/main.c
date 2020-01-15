eaeb964f457ab56c62c59bbd9c9302f567767f93
Daniyar Yusuf <dyusuf@e1z1r2p11.42.us.org>
2020-01-14 18:36:54 -0800

    ex09

--- d05/main.c
+++ d05/main.c
@@ -11,6 +11,7 @@ char 	*ft_strstr(char *str, char *to_find);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
 char 	*ft_strupcase(char *str);
+char 	*ft_strlowcase(char *str);
 
 
 int		main(void)
@@ -26,6 +27,6 @@ int		main(void)
 //	printf("%s", ft_strncpy(dest, src, 4));
 //	printf("\n%s", ft_strstr(src, a));
 //	printf("\n%d", ft_strncmp(src, a, 8));
-	printf("\n%s", ft_strupcase(a));
+	printf("\n%s", ft_strlowcase(a));
 	return (0);
 }
