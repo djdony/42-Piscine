f4df2e0aa7a2b8c77d91f128a9a791b744fd7a85
Daniyar Yusuf <dyusuf@e1z1r2p15.42.us.org>
2020-01-15 17:54:34 -0800

    ok

---
+++ d06/ex01/ft_print_program_name.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyusuf <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/15 17:28:23 by dyusuf            #+#    #+#             */
+/*   Updated: 2020/01/15 17:40:55 by dyusuf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_program_name(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	ft_print_program_name(argv[0]);
+	return (0);
+}
---
+++ d06/ex02/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyusuf <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/15 17:42:59 by dyusuf            #+#    #+#             */
+/*   Updated: 2020/01/15 17:46:50 by dyusuf           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_params(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_print_params(argv[i]);
+		i++;
+	}
+	return (0);
+}
